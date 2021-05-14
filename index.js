abc3907fb15ecabb06cea271f8da8d0e825b1442
megan <megan.schertz@gmail.com>
2021-05-14 12:53:59 -0500

    commit tree 8e982ae373884a9b9b94824a3e61b05edf973115

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+    //...
+  ]
+function superbowlWin(record) {
+    const foundRecord = record.find( w => { return w.result === "W"})
+    if (foundRecord){
+        return foundRecord.year
+    }
+}
